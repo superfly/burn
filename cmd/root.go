bd66d70ae8808e2f6e11d1ea8c8edeb6a87669a9
Jerome Gravel-Niquet <jeromegn@gmail.com>
2017-10-20 15:59:04 -0400

    blob of code

--- cmd/root.go
+++ cmd/root.go
@@ -12,31 +12,34 @@ import (
 	"net/url"
 	"os"
 	"os/signal"
+	"sort"
 	"sync"
 	"sync/atomic"
 	"time"
 
 	"github.com/spf13/cobra"
-	"github.com/vbauerster/mpb"
-	"github.com/vbauerster/mpb/decor"
 
 	"github.com/montanaflynn/stats"
 	"github.com/olekukonko/tablewriter"
 
 	"github.com/abiosoft/semaphore"
+
+	"github.com/logrusorgru/aurora"
 )
 
 func init() {
 	RootCmd.PersistentFlags().Bool("verbose", false, "verbose output")
 	RootCmd.Flags().IntP("concurrency", "c", 10, "number of concurrent requests")
 	RootCmd.Flags().StringP("duration", "d", "60s", "duration for the test")
+
+	RootCmd.Flags().BoolP("insecure-skip-verify", "k", true, "allow insecure TLS certificates (self-signed, wrong hostname, etc. default: true)")
+	RootCmd.Flags().Bool("disable-keepalives", false, "disable reusing connections via keepalives (default: false)")
+	RootCmd.Flags().Bool("resume-tls", false, "allow resuming TLS (default: false)")
 }
 
 func Execute() {
 	RootCmd.Execute()
 }
-
-const pbPrefix = "Burning..."
 
 var timeSpent time.Duration
 
@@ -48,7 +51,11 @@ var (
 	requestTimes      = NewConcurrentFloat64Slice()
 	requestsCount     int64
 
-	tlsCiphersUsed = []uint16{}
+	tlsCiphersUsed = NewConcurrentUint16Slice()
+	tlsReusedCount int64
+
+	errors   = NewConcurrentErrorSlice()
+	statuses = NewConcurrentIntSlice()
 )
 
 var RootCmd = &cobra.Command{
@@ -78,103 +85,142 @@ var RootCmd = &cobra.Command{
 			panic(err)
 		}
 
-		fmt.Printf("burning on %s\n", u)
-
-		p := mpb.New()
-		pb := p.AddBar(int64(dur.Seconds()),
-			mpb.PrependDecorators(
-				decor.StaticName(pbPrefix, len(pbPrefix), 0),
-			),
-			mpb.AppendDecorators(
-				// Percentage decorator with minWidth and no extra config
-				decor.Percentage(5, 0),
-			),
-		)
-
 		timeout := time.After(dur)
 		sigs := make(chan os.Signal, 1)
 		signal.Notify(sigs, os.Interrupt)
 
 		cancel := make(chan struct{})
 
-		ticker := time.NewTicker(time.Second)
-		go func() {
-			for _ = range ticker.C {
-				pb.Incr(1)
-			}
-		}()
+		var start time.Time
+
+		fmt.Printf("\n%s %s for %s\n", aurora.Red("Burning"), u, aurora.Bold(d))
 
 		sem := semaphore.New(c)
 
-		var start time.Time
+		transport := getTransport(cmd)
 		go func() {
 			start = time.Now()
 			for {
 				sem.Acquire()
 				go func() {
+					var reqStart time.Time
 					defer sem.Release()
+					defer func() {
+						go responseTimes.Append(time.Since(reqStart).Seconds())
+					}()
 					req := &http.Request{
 						URL:    u,
 						Header: make(http.Header),
 					}
 					ctx := httptrace.WithClientTrace(context.Background(), getClientTracer())
-					start := time.Now()
+					reqStart = time.Now()
 					res, err := transport.RoundTrip(req.WithContext(ctx))
-					atomic.AddInt64(&requestsCount, 1)
+					go atomic.AddInt64(&requestsCount, 1)
 					if err != nil {
-						cancel <- struct{}{}
-						fmt.Printf("cancelled due to error: %s\n", err)
+						go errors.Append(err)
+						return
 					}
-					if res != nil {
-						io.Copy(ioutil.Discard, res.Body)
+					go statuses.Append(res.StatusCode)
+
+					if res.Body != nil {
+						if _, err := io.Copy(ioutil.Discard, res.Body); err != nil {
+							go errors.Append(err)
+						}
+						if err := res.Body.Close(); err != nil {
+							go errors.Append(err)
+						}
 					}
-					responseTimes.Append(time.Since(start).Seconds())
+
 				}()
 			}
 		}()
 
 		select {
 		case <-timeout:
-			fmt.Println("all done")
 			break
 		case <-sigs:
-			fmt.Println("interrupted")
 			break
 		case <-cancel:
-			fmt.Println("cancelled!")
+			fmt.Println("Burn cancelled!")
 			break
 		}
 
 		timeSpent = time.Since(start)
 
-		ticker.Stop()
-		p.Stop()
+		time.Sleep(100 * time.Millisecond)
 
+		fmt.Println(aurora.Green("\nStatistical Analysis:"))
 		renderStats()
+
+		fmt.Println(aurora.Cyan("\nMeta:"))
 		renderMeta()
 
-		fmt.Println("ciphers used:")
-		ciphersCount := map[string]int64{}
-		for _, cipher := range tlsCiphersUsed {
-			ciphersCount[tlsCiphersList[cipher]] = ciphersCount[tlsCiphersList[cipher]] + 1
+		if len(tlsCiphersUsed.GetItems()) > 0 {
+			fmt.Println(aurora.Blue("\nCiphers:"))
+			ciphersCount := map[string]int64{}
+			for _, cipher := range tlsCiphersUsed.GetItems() {
+				ciphersCount[tlsCiphersList[cipher]] = ciphersCount[tlsCiphersList[cipher]] + 1
+			}
+			for n, count := range ciphersCount {
+				fmt.Printf("%s => %d\n", n, count)
+			}
 		}
 
-		fmt.Printf("%+v\n", ciphersCount)
+		fmt.Println(aurora.Red("\nErrors:"))
+		fmt.Printf("%d errors.\n", len(errors.GetItems()))
+		// if len(errors.GetItems()) > 0 {
+		errorsCount := map[string]int64{}
+		for _, err := range errors.GetItems() {
+			errorsCount[err.Error()] = errorsCount[err.Error()] + 1
+		}
+
+		for n, count := range errorsCount {
+			fmt.Printf("%s => %d\n", n, count)
+		}
+		// }
+
+		fmt.Println("")
 
 		os.Exit(0)
 	},
 }
 
 func renderStats() {
-	data := [][]string{}
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetAutoFormatHeaders(false)
+	table.SetHeader([]string{"Metric", "p50", "p75", "p95", "p99", "Min", "Mean", "Max", "Std. Dev"})
+	table.SetHeaderColor(nil, nil, nil, nil, nil, nil, nil, nil, tablewriter.Color(tablewriter.FgHiBlackColor))
+	table.SetColumnColor(nil, nil, nil, nil, nil, nil, nil, nil, tablewriter.Color(tablewriter.FgHiBlackColor))
 
-	for name, cs := range map[string]*ConcurrentFloat64Slice{
+	m := map[int]string{
+		0: "Connect",
+		1: "TLS Handshake",
+		2: "Request fully written",
+		3: "Response fully read",
+	}
+
+	var keys []int
+	for k := range m {
+		keys = append(keys, k)
+	}
+
+	csm := map[string]*ConcurrentFloat64Slice{
 		"Connect":               connectTimes,
 		"TLS Handshake":         tlsHandshakeTimes,
 		"Request fully written": requestTimes,
 		"Response fully read":   responseTimes,
-	} {
+	}
+
+	sort.Ints(keys)
+
+	for _, k := range keys {
+		name := m[k]
+		cs := csm[name]
 		d := []string{name}
+		if len(cs.GetItems()) == 0 {
+			continue
+		}
+
 		for _, p := range []int{50, 75, 95, 99} {
 			if pf, err := stats.Percentile(cs.GetItems(), float64(p)); err != nil {
 				panic(err)
@@ -187,15 +233,53 @@ func renderStats() {
 				}
 			}
 		}
-		data = append(data, d)
+
+		if min, err := stats.Min(cs.GetItems()); err != nil {
+			panic(err)
+		} else {
+			mins := fmt.Sprintf("%f", min)
+			if mind, err := time.ParseDuration(mins + "s"); err != nil {
+				panic(err)
+			} else {
+				d = append(d, mind.String())
+			}
+		}
+
+		if mean, err := stats.Mean(cs.GetItems()); err != nil {
+			panic(err)
+		} else {
+			means := fmt.Sprintf("%f", mean)
+			if meand, err := time.ParseDuration(means + "s"); err != nil {
+				panic(err)
+			} else {
+				d = append(d, meand.String())
+			}
+		}
+
+		if max, err := stats.Max(cs.GetItems()); err != nil {
+			panic(err)
+		} else {
+			maxs := fmt.Sprintf("%f", max)
+			if maxd, err := time.ParseDuration(maxs + "s"); err != nil {
+				panic(err)
+			} else {
+				d = append(d, maxd.String())
+			}
+		}
+
+		if stddev, err := stats.StandardDeviation(cs.GetItems()); err != nil {
+			panic(err)
+		} else {
+			stddevs := fmt.Sprintf("%f", stddev)
+			if stddevd, err := time.ParseDuration(stddevs + "s"); err != nil {
+				panic(err)
+			} else {
+				d = append(d, stddevd.String())
+			}
+		}
+		table.Append(d)
 	}
 
-	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"Metric", "p50", "p75", "p95", "p99"})
-
-	for _, v := range data {
-		table.Append(v)
-	}
 	table.Render() // Send output
 }
 
@@ -204,6 +288,7 @@ func renderMeta() {
 		[]string{"Requests Count", fmt.Sprintf("%d", requestsCount)},
 		[]string{"Time spent", timeSpent.String()},
 		[]string{"RPS", fmt.Sprintf("%f", float64(requestsCount)/timeSpent.Seconds())},
+		[]string{"TLS Resumed", fmt.Sprintf("%d", atomic.LoadInt64(&tlsReusedCount))},
 	}
 
 	table := tablewriter.NewWriter(os.Stdout)
@@ -212,25 +297,41 @@ func renderMeta() {
 	table.Render()
 }
 
-var transport = &http.Transport{
-	DisableKeepAlives:     true,
-	TLSHandshakeTimeout:   10 * time.Second,
-	ExpectContinueTimeout: 1 * time.Second, // I guess?
-	ResponseHeaderTimeout: 60 * time.Second,
+func getTransport(cmd *cobra.Command) *http.Transport {
+	c, err := cmd.Flags().GetInt("concurrency")
+	if err != nil {
+		panic(err)
+	}
 
-	DialContext: (&net.Dialer{
-		Timeout:   5 * time.Second,
-		DualStack: true,
-	}).DialContext,
+	insecureSkipVerify, err := cmd.Flags().GetBool("insecure-skip-verify")
+	if err != nil {
+		panic(err)
+	}
 
-	TLSClientConfig: &tls.Config{
-		InsecureSkipVerify:     true,
-		SessionTicketsDisabled: true,
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256, // http2
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+	disableKeepalives, err := cmd.Flags().GetBool("disable-keepalives")
+	if err != nil {
+		panic(err)
+	}
+
+	return &http.Transport{
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second, // I guess?
+		ResponseHeaderTimeout: 60 * time.Second,
+
+		DisableKeepAlives:   disableKeepalives,
+		MaxIdleConns:        1024,  // arbitrary
+		MaxIdleConnsPerHost: c * 2, // as many as we want to concurrently run
+
+		DialContext: (&net.Dialer{
+			Timeout:   5 * time.Second,
+			DualStack: true,
+		}).DialContext,
+
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: insecureSkipVerify,
+			ClientSessionCache: tls.NewLRUClientSessionCache(1024 * 10),
 		},
-	},
+	}
 }
 
 func getClientTracer() *httptrace.ClientTrace {
@@ -250,17 +351,20 @@ func getClientTracer() *httptrace.ClientTrace {
 			connectStart = time.Now()
 		},
 		ConnectDone: func(network, addr string, err error) {
-			connectTimes.Append(time.Since(connectStart).Seconds())
+			go connectTimes.Append(time.Since(connectStart).Seconds())
 		},
 		TLSHandshakeStart: func() {
 			tlsStart = time.Now()
 		},
 		TLSHandshakeDone: func(connState tls.ConnectionState, err error) {
-			tlsHandshakeTimes.Append(time.Since(tlsStart).Seconds())
-			tlsCiphersUsed = append(tlsCiphersUsed, connState.CipherSuite)
+			go tlsHandshakeTimes.Append(time.Since(tlsStart).Seconds())
+			go tlsCiphersUsed.Append(connState.CipherSuite)
+			if connState.DidResume {
+				go atomic.AddInt64(&tlsReusedCount, 1)
+			}
 		},
 		WroteRequest: func(info httptrace.WroteRequestInfo) {
-			requestTimes.Append(time.Since(reqStart).Seconds())
+			go requestTimes.Append(time.Since(reqStart).Seconds())
 		},
 	}
 }
@@ -287,29 +391,93 @@ func (cs *ConcurrentFloat64Slice) GetItems() []float64 {
 	return cs.items
 }
 
+type ConcurrentErrorSlice struct {
+	sync.RWMutex
+	items []error
+}
+
+func NewConcurrentErrorSlice() *ConcurrentErrorSlice {
+	return &ConcurrentErrorSlice{items: make([]error, 0)}
+}
+
+func (cs *ConcurrentErrorSlice) Append(f error) {
+	cs.Lock()
+	defer cs.Unlock()
+
+	cs.items = append(cs.items, f)
+}
+
+func (cs *ConcurrentErrorSlice) GetItems() []error {
+	cs.RLock()
+	defer cs.RUnlock()
+	return cs.items
+}
+
+type ConcurrentIntSlice struct {
+	sync.RWMutex
+	items []int
+}
+
+func NewConcurrentIntSlice() *ConcurrentIntSlice {
+	return &ConcurrentIntSlice{items: make([]int, 0)}
+}
+
+func (cs *ConcurrentIntSlice) Append(f int) {
+	cs.Lock()
+	defer cs.Unlock()
+	cs.items = append(cs.items, f)
+}
+
+func (cs *ConcurrentIntSlice) GetItems() []int {
+	cs.RLock()
+	defer cs.RUnlock()
+	return cs.items
+}
+
+type ConcurrentUint16Slice struct {
+	sync.RWMutex
+	items []uint16
+}
+
+func NewConcurrentUint16Slice() *ConcurrentUint16Slice {
+	return &ConcurrentUint16Slice{items: make([]uint16, 0)}
+}
+
+func (cs *ConcurrentUint16Slice) Append(f uint16) {
+	cs.Lock()
+	defer cs.Unlock()
+	cs.items = append(cs.items, f)
+}
+
+func (cs *ConcurrentUint16Slice) GetItems() []uint16 {
+	cs.RLock()
+	defer cs.RUnlock()
+	return cs.items
+}
+
 var tlsCiphersList = map[uint16]string{
-	tls.TLS_RSA_WITH_RC4_128_SHA:                "TLS_RSA_WITH_RC4_128_SHA",
-	tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA:           "TLS_RSA_WITH_3DES_EDE_CBC_SHA",
-	tls.TLS_RSA_WITH_AES_128_CBC_SHA:            "TLS_RSA_WITH_AES_128_CBC_SHA",
-	tls.TLS_RSA_WITH_AES_256_CBC_SHA:            "TLS_RSA_WITH_AES_256_CBC_SHA",
-	tls.TLS_RSA_WITH_AES_128_CBC_SHA256:         "TLS_RSA_WITH_AES_128_CBC_SHA256",
-	tls.TLS_RSA_WITH_AES_128_GCM_SHA256:         "TLS_RSA_WITH_AES_128_GCM_SHA256",
-	tls.TLS_RSA_WITH_AES_256_GCM_SHA384:         "TLS_RSA_WITH_AES_256_GCM_SHA384",
-	tls.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA:        "TLS_ECDHE_ECDSA_WITH_RC4_128_SHA",
-	tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA:    "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA",
-	tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA:    "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA",
-	tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA:          "TLS_ECDHE_RSA_WITH_RC4_128_SHA",
-	tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA:     "TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA",
-	tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA:      "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
-	tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA:      "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
-	tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256: "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256",
-	tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256:   "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256",
-	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256:   "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
-	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256: "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
-	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384:   "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
-	tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384: "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
-	tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305:    "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305",
-	tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305:  "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305",
+	tls.TLS_RSA_WITH_RC4_128_SHA:                "RSA w/ RC4_128_SHA",
+	tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA:           "RSA w/ 3DES_EDE_CBC_SHA",
+	tls.TLS_RSA_WITH_AES_128_CBC_SHA:            "RSA w/ AES_128_CBC_SHA",
+	tls.TLS_RSA_WITH_AES_256_CBC_SHA:            "RSA w/ AES_256_CBC_SHA",
+	tls.TLS_RSA_WITH_AES_128_CBC_SHA256:         "RSA w/ AES_128_CBC_SHA256",
+	tls.TLS_RSA_WITH_AES_128_GCM_SHA256:         "RSA w/ AES_128_GCM_SHA256",
+	tls.TLS_RSA_WITH_AES_256_GCM_SHA384:         "RSA w/ AES_256_GCM_SHA384",
+	tls.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA:        "ECDHE ECDSA w/ RC4_128_SHA",
+	tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA:    "ECDHE ECDSA w/ AES_128_CBC_SHA",
+	tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA:    "ECDHE ECDSA w/ AES_256_CBC_SHA",
+	tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA:          "ECDHE RSA w/ RC4_128_SHA",
+	tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA:     "ECDHE RSA w/ 3DES_EDE_CBC_SHA",
+	tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA:      "ECDHE RSA w/ AES_128_CBC_SHA",
+	tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA:      "ECDHE RSA w/ AES_256_CBC_SHA",
+	tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256: "ECDHE ECDSA w/ AES_128_CBC_SHA256",
+	tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256:   "ECDHE RSA w/ AES_128_CBC_SHA256",
+	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256:   "ECDHE RSA w/ AES_128_GCM_SHA256",
+	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256: "ECDHE ECDSA w/ AES_128_GCM_SHA256",
+	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384:   "ECDHE RSA w/ AES_256_GCM_SHA384",
+	tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384: "ECDHE ECDSA w/ AES_256_GCM_SHA384",
+	tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305:    "ECDHE RSA w/ CHACHA20_POLY1305",
+	tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305:  "ECDHE ECDSA w/ CHACHA20_POLY1305",
 
 	// TLS_FALLBACK_SCSV isn't a standard cipher suite but an indicator
 	// that the client is doing version fallback. See
