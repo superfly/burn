1c4bb05f5c3d675c35f6b245f444c0d061e0fda6
Jerome Gravel-Niquet <jeromegn@gmail.com>
2019-08-07 16:00:29 -0400

    remove comment and Gopkg shit

--- main.go
+++ main.go
@@ -8,9 +8,6 @@ import (
 )
 
 func main() {
-	// if err := agent.Listen(agent.Options{}); err != nil {
-	// 	log.Fatal(err)
-	// }
 	if err := cmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
